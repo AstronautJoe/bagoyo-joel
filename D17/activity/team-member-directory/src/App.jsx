ebe6e0e101c5abfd9e0b2cd779aea3e33a29e01e
Joel Bagoyo <jpbagoyo@gmail.com>
2024-11-28 22:01:25 +0800

    feat: add base for day 17 activity team-member-directory

---
+++ D17/activity/team-member-directory/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import DirectoryNavbar from './components/DirectoryNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <DirectoryNavbar />
+    </>
+  );
+}
+
+export default App;

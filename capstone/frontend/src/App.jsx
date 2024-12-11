ad22e46d2a30395af77b0b54cf05b0f31dde89dc
Joel Bagoyo <jpbagoyo@gmail.com>
2024-12-11 20:13:54 +0800

    style: add base style version

--- capstone/frontend/src/App.jsx
+++ capstone/frontend/src/App.jsx
@@ -1,10 +1,10 @@
 export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-neutral">
-      <h1 className="text-4xl font-bold text-primary mb-4">
+    <div className="flex flex-col items-center justify-center h-screen bg-primary">
+      <h1 className="text-4xl font-bold text-secondary mb-4">
         DaisyUI Theme Test
       </h1>
-      <button className="btn btn-warning">Click Me!</button>
+      <button className="btn btn-success">Click Me!</button>
     </div>
   );
 }
--- capstone/frontend/tailwind.config.js
+++ capstone/frontend/tailwind.config.js
@@ -15,9 +15,9 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: '#2D3748', // Dark gray for primary UI elements
-          secondary: '#A0AEC0', // Cool gray for secondary UI elements
-          accent: '#38B2AC', // Teal for accent highlights
+          primary: '#537AF3', // Dark gray for primary UI elements
+          secondary: '#2A2D34', // Cool gray for secondary UI elements
+          accent: '#A8DADC', //accent highlights
           neutral: '#EDF2F7', // Light gray for neutral backgrounds
           'base-100': '#FFFFFF', // Clean white for base
           'base-content': '#1A202C', // Dark gray for text

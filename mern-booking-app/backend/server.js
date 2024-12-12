7ecc5930e9a01f0e3db49f15e1fd2ccfd2a0a10f
Joel Bagoyo <jpbagoyo@gmail.com>
2024-12-12 19:59:03 +0800

    docs: arranged and added notes

--- mern-booking-app/backend/server.js
+++ mern-booking-app/backend/server.js
@@ -1,26 +1,40 @@
- import express from 'express'
- import cors from 'cors'
- import "dotenv/config";
- import mongoose from 'mongoose';
- import userRoutes from './routes/userRoutes.js';
-import authRoutes from './routes/authRoutes.js'
-import myHotelRoutes from './routes/myHotelRoutes.js'
-import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from 'cloudinary'
+// Import Modules
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'; // Loads environment variables from .env file
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser'; // Parses cookies for request handling
+import { v2 as cloudinary } from 'cloudinary';
 
+// Import routes
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import myHotelRoutes from './routes/myHotelRoutes.js';
+
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-mongoose.connect(process.env.MONGO_URI);
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+// Initialize Express Application
 const app = express();
 
-app.use(cookieParser());
-
-app.use(express.json());
+// Middleware
+app.use(cookieParser()); // Turns the long string of cookies into an easy to handle Javascript Object
+app.use(express.json()); // Parse incoming request bodies as JSON and URL-encoded
 app.use(express.urlencoded({ extended: true }));
+// Enable CORS for the frontend URL
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -28,11 +42,12 @@ app.use(
   })
 );
 
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/my-hotels', myHotelRoutes);
 
-
- app.listen(3000, () => {
-    console.log("server running on localhost 3000")
- })
+// Start the server
+app.listen(3000, () => {
+  console.log('server running on localhost 3000');
+});

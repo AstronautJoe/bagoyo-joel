fc81a01c787142b47818f17b8fabf15feb6df6c9
Joel Bagoyo <jpbagoyo@gmail.com>
2024-12-23 23:47:33 +0800

    style: add mobile responsiveness for ability card component

--- capstone/frontend/src/components/ChampionAbilitiesSection.jsx
+++ capstone/frontend/src/components/ChampionAbilitiesSection.jsx
@@ -10,7 +10,7 @@ const ChampionAbilitiesSection = ({ championData }) => {
         {championData.name.toUpperCase()}&apos;S ABILITIES
       </h2>
       <AbilityCard ability={passive} isPassive={1} />
-      <div className="grid grid-cols-2 gap-5 mt-5">
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-5 mt-5">
         {abilities.map((ability, key) => (
           <AbilityCard key={key} ability={ability} />
         ))}
